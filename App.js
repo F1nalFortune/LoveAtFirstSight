544ee1b29c75631dfd697907a294948e8d0da22f
Brian Phelps Jr <phelpsb92@gmail.com>
2021-03-08 21:06:32 -0500

    font awesome icons for navigation

--- App.js
+++ App.js
@@ -12,6 +12,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faVideo, faUser } from '@fortawesome/free-solid-svg-icons'
+
 const Tab = createBottomTabNavigator();
 const VideoStack = createStackNavigator();
 const SettingsStack = createStackNavigator();
@@ -43,16 +46,39 @@ function SettingsStackScreen() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Video') {
+              iconName = faVideo;
+            } else if (route.name === 'Settings') {
+              iconName = faUser;
+            }
+            // You can return any component that you like here!
+            return <FontAwesomeIcon icon={ iconName } size={25}/>;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: 'white',
+          inactiveBackgroundColor: 'black',
+          activeBackgroundColor: '#272727',
+          style:{
+            height: 65,
+            zIndex: 1000
+          }
+        }}>
         <Tab.Screen
-          name="Home"
+          name="Video"
           component={VideoStackScreen}
-          options={{ tabBarLabel: 'Home!' }}
+          options={{ tabBarLabel: 'Video' }}
         />
         <Tab.Screen
           name="Settings"
           component={SettingsStackScreen}
-          options={{ tabBarLabel: 'Settings!' }}
+          options={{ tabBarLabel: 'Settings' }}
         />
       </Tab.Navigator>
     </NavigationContainer>
